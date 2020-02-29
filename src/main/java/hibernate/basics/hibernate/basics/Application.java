bee8725c0b0d04d63bc2eb7f6ac47e558e1f7024
Bogu <adambozek1@gmail.com>
2020-02-29 23:01:36 +0100

    Rollback to simple text interface because of errors.

--- src/main/java/hibernate/basics/hibernate/basics/Application.java
+++ src/main/java/hibernate/basics/hibernate/basics/Application.java
@@ -7,11 +7,56 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
+import java.util.Scanner;
+
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class Application {
 
+
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		SessionFactory factory = new Configuration().addAnnotatedClass(PersonEntity.class).buildSessionFactory();
+		Session session = factory.getCurrentSession();
+
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.println("1. Create new person");
+		System.out.println("2. Read person by id");
+		System.out.println("3. Update employee");
+		System.out.println("4. Delete employee");
+		System.out.println("5. Exit");
+		System.out.println("=====================");
+
+		int menuChoice = scanner.nextInt();
+		if(menuChoice == 1) {
+			String skipCatcher = scanner.nextLine();
+			System.out.println("Enter name:");
+			String name = scanner.nextLine();
+			System.out.println("Enter last name:");
+			String lastName = scanner.nextLine();
+			System.out.println("Enter email:");
+			String email = scanner.nextLine();
+
+			PersonEntity person = new PersonEntity();
+			person.setFirstName(name);
+			person.setLastName(lastName);
+			person.setEmail(email);
+			session.getTransaction().begin();
+			session.persist(person);
+			session.getTransaction().commit();
+		}
+		else if(menuChoice == 2) {
+			System.exit(0);
+		}
+		else if(menuChoice == 3) {
+			System.exit(0);
+		}
+		else if(menuChoice == 4) {
+			System.exit(0);
+		}
+		else if(menuChoice == 5) {
+			System.exit(0);
+		}
+		//SpringApplication.run(Application.class, args);
 	}
 
 }
--- src/main/java/hibernate/basics/hibernate/basics/CRUD_Controller.java
+++ src/main/java/hibernate/basics/hibernate/basics/CRUD_Controller.java
@@ -1,56 +1,69 @@
-package hibernate.basics.hibernate.basics;
-
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.cfg.Configuration;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-@Controller
-public class CRUD_Controller {
-    private SessionFactory factory = new Configuration()
-            .addAnnotatedClass(PersonEntity.class)
-            .buildSessionFactory();
-    private Session session = factory.getCurrentSession();
-
-    @RequestMapping("/create")
-    public String create(@ModelAttribute("personEntity") PersonEntity person, Model model){
-        try{
-            PersonEntity personEnt = new PersonEntity();
-            personEnt.setLastName(person.getLastName());
-            personEnt.setFirstName(person.getFirstName());
-            personEnt.setEmail(person.getEmail());
-            model.addAttribute(person);
-            this.session.beginTransaction();
-            this.session.save(personEnt);
-            this.session.getTransaction().commit();
-        }
-        finally{
-            factory.close();
-        }
-        return "create";
-    }
-
-    @RequestMapping("/read")
-    public String read(@ModelAttribute("personEntity") PersonEntity person, Model model){
-        return "read";
-    }
-
-    @RequestMapping("/readEffect")
-    public String readEffect(@ModelAttribute("personEntity") PersonEntity person, Model model){
-        return "readEffect";
-    }
-
-    @RequestMapping("/delete")
-    public String delete(@ModelAttribute("personEntity") PersonEntity person, Model model){
-        return "delete";
-    }
-
-    @RequestMapping("/update")
-    public String update(@ModelAttribute("personEntity") PersonEntity person, Model model){
-        return "update";
-    }
-
-}
+//package hibernate.basics.hibernate.basics;
+//
+//import org.hibernate.Session;
+//import org.hibernate.SessionFactory;
+//import org.hibernate.cfg.Configuration;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.ui.Model;
+//import org.springframework.web.bind.annotation.ModelAttribute;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.data.jpa.util.*;
+//
+//import javax.persistence.EntityManager;
+//import javax.persistence.EntityManagerFactory;
+//import javax.persistence.Persistence;
+//
+//@Controller
+//public class CRUD_Controller {
+//    private SessionFactory factory;
+//    private Session session;
+//
+//    public EntityManagerFactory entityManagerFactory;
+//    public EntityManager entityManager;
+//
+//    public CRUD_Controller(){
+//        this.factory = new Configuration().addAnnotatedClass(PersonEntity.class).buildSessionFactory();
+//        this.session = factory.getCurrentSession();
+////        this.entityManagerFactory = Persistence.createEntityManagerFactory("hibernate_test");
+////        this.entityManager = entityManagerFactory.createEntityManager();
+//    }
+//
+//    @RequestMapping("/create")
+//    public String create(@ModelAttribute("personEntity") PersonEntity person, Model model){
+//       // try{
+//            PersonEntity personEnt = new PersonEntity();
+//            personEnt.setLastName(person.getLastName());
+//            personEnt.setFirstName(person.getFirstName());
+//            personEnt.setEmail(person.getEmail());
+//            model.addAttribute(person);
+//           session.getTransaction().begin();
+//           session.persist(personEnt);
+//           session.getTransaction().commit();
+////        }
+////        finally{
+////            factory.close();
+////        }
+//        return "create";
+//    }
+//
+//    @RequestMapping("/read")
+//    public String read(@ModelAttribute("personEntity") PersonEntity person, Model model){
+//        return "read";
+//    }
+//
+//    @RequestMapping("/readEffect")
+//    public String readEffect(@ModelAttribute("personEntity") PersonEntity person, Model model){
+//        return "readEffect";
+//    }
+//
+//    @RequestMapping("/delete")
+//    public String delete(@ModelAttribute("personEntity") PersonEntity person, Model model){
+//        return "delete";
+//    }
+//
+//    @RequestMapping("/update")
+//    public String update(@ModelAttribute("personEntity") PersonEntity person, Model model){
+//        return "update";
+//    }
+//
+//}
